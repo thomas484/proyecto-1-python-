49f5717cbee4e7e6986f14f6108296d284145d73
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-04 16:41:15 -0300

    fin programa básico

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,7 @@
 print("Hello world python!")
 numero = input("ingrese numero: ")
-print(int(numero)+9)
+print(int(numero))
+#variable booleana 
+mi_booleano = True 
+print(mi_booleano) #imprime true 
+print(4 == 4) #imprime un true 
