0916e6638c9e8450da602a9820dec14e498b5887
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-04 16:51:28 -0300

    ifelse

---
+++ sentencia if.py
@@ -0,0 +1,7 @@
+# if condicion:
+#    codigo, al principio de la linea esta indentado 
+x = input("Ingrese un numero: ") 
+if int(x) > 5:
+    print(int(x), "es mayor que 5")
+else:
+    print(int(x), "es menor que 5")
