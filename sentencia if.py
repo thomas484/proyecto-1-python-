4dd91fba17c73bc3af8ad0bfb24562b7baedbc5d
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-04 17:04:47 -0300

    actualizado

--- sentencia if.py
+++ sentencia if.py
@@ -1,7 +1,7 @@
 # if condicion:
 #    codigo, al principio de la linea esta indentado 
 x = input("Ingrese un numero: ") 
-if int(x) > 5:
+if int(x) > 5: #para comparar una entrada por teclado primero se tiene que pasar a int o float 
     print(int(x), "es mayor que 5")
 else:
     print(int(x), "es menor que 5")
