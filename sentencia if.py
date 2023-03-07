3bec45d6e1a88fa8e56a58487bf93122756aea10
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-07 03:28:24 -0300

    libreria math

--- sentencia if.py
+++ sentencia if.py
@@ -1,6 +1,6 @@
 # if condicion:
 #    codigo, al principio de la linea esta indentado 
-
+import math 
 x = input("Ingrese un numero: ") 
 if int(x) > 5: #para comparar una entrada por teclado primero se tiene que pasar a int o float 
     print(int(x), "es mayor que 5")
@@ -10,3 +10,4 @@ else:
 fahrenheit = input("Ingrese la temperatura en grados Fahrenheit: ") #para que no entre dentro del else debe ir un espacio antes 
 celcius = (float(fahrenheit) - 32) * 5/9
 print("En Celcius: ", celcius)
+print(math.pi)
