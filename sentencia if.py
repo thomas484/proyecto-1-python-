839a17966431d9ef12e8205169e20195160da679
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-07 03:08:09 -0300

    agregado conversor de temp

--- sentencia if.py
+++ sentencia if.py
@@ -1,7 +1,12 @@
 # if condicion:
 #    codigo, al principio de la linea esta indentado 
+
 x = input("Ingrese un numero: ") 
 if int(x) > 5: #para comparar una entrada por teclado primero se tiene que pasar a int o float 
     print(int(x), "es mayor que 5")
 else:
     print(int(x), "es menor que 5")
+
+fahrenheit = input("Please Enter Fahrenheit value: ")
+celcius = (float(fahrenheit) - 32) * 5/9
+print("Celsius equivalent is: ", celcius)
