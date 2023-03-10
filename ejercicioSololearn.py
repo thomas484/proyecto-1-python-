53c1879a9b128407be9c2eb8fa1278338abbfe99
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-10 19:13:18 -0300

    modif 1

--- ejercicioSololearn.py
+++ ejercicioSololearn.py
@@ -9,3 +9,14 @@ elif year % 400 == 0:
             print("Leap year")
 elif year % 400 != 0:
     print("Not a leap year")   
+#purificador de oro 
+purity = float(input())
+#your code goes here
+if purity >= 99.9:
+    print("24K")
+if purity < 99.9 and purity >= 91.7:
+    print("22K")
+if purity < 91.7 and purity >= 83.3:
+    print("20K")
+if purity < 83.3:
+    print("18K")    
