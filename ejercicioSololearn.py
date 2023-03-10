0f4acd9538f441312d2faec8c11ec3aaf640e25f
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-10 17:18:19 -0300

    año biciesto

---
+++ ejercicioSololearn.py
@@ -0,0 +1,11 @@
+#el objetivo del codigo es verificar si el año es biciesto 
+year = int(input())
+#your code goes here
+if year % 4 != 0:
+    print("Not a leap year") 
+elif year % 100 != 0:
+    print("Leap year")
+elif year % 400 == 0:
+            print("Leap year")
+elif year % 400 != 0:
+    print("Not a leap year")   
