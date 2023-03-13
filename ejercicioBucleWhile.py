ca45a6a64c2ef5c26a75c70affcb702a4fa7f424
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-13 01:18:12 -0300

    v1

--- ejercicioBucleWhile.py
+++ ejercicioBucleWhile.py
@@ -2,4 +2,16 @@ x = 0
 while x < 10:
     print("valor de x: " ,str(x))
     x+=1
+#juego de puntaje dependiendo el string que ingrese el usuario 
 
+score = 100
+x = 0
+while x<4:
+    shot = input("Ingrese su tiro: " )
+    if shot == "hit":
+        score+=10
+    elif shot == "miss":
+        score-=20
+    
+    x+=1 
+print(score)
