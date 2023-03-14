53b576352efec6e45525a0ffab46af0181ea0609
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-14 01:25:45 -0300

    listas con dias

--- listas.py
+++ listas.py
@@ -1,2 +1,8 @@
 #ejemplos de listas con ejercicios 
-list 
+# las lista son usadas para almacenar valores o ítems 
+# se crean usando corchetes y comas para separar cada miembro 
+dias =["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+i=0
+while i < 7:
+    print("Hoy es: ", dias[i])
+    i+=1
