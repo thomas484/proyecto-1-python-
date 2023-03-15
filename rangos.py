cb73e6440823bbb99f680ee4a2a91a96b5a40615
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-15 02:33:55 -0300

    rangos v2

--- rangos.py
+++ rangos.py
@@ -1,1 +1,8 @@
 #funcion range()
+numeros = list(range(10))
+for i in numeros:
+ print(str(numeros[i]) + "!") # imprime los números del 0 al 11, range genera numeros en la forma de dato lista
+#0!
+#1! 
+#2! ...
+#9!
