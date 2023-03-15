1283b08067390d5b8aca1c8f4fca585dd17487e7
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-15 02:01:47 -0300

    ejercicio 2

--- bucles for.py
+++ bucles for.py
@@ -2,3 +2,11 @@
 palabras = ["Hola", "Adiós", "Mundo"]
 for f in palabras: #para f en palabras copia y agrega un !
     print(f + "!")
+# ejemplo de recorrer un string 
+cuenta = 0
+palabra = str(input("Ingrese un string: "))
+l = str(input("Ingrese el caracter a buscar: "))
+for c in palabra:
+    if(c == l):
+        cuenta+=1
+print("Coincidencias con la letra a: ", int(cuenta))
