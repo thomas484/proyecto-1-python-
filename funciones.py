ca642f5259b963141ab432c48443452aea5c3b5f
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-17 02:33:35 -0300

    funcion len

--- funciones.py
+++ funciones.py
@@ -1,4 +1,6 @@
 #funciones en python 
-numeros = list(range(10,70))
+numeros = list(range(10))
 for i in numeros:
     print(numeros[i])
+letras = ["a", "b", "n", "s", "r", "h"]
+print("Longitud de la cadena:",len(letras))
