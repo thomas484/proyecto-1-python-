836e4cb758c5072a4ebf19a10f9b6e28d3d2a367
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-17 02:40:44 -0300

    funcion append

--- funciones.py
+++ funciones.py
@@ -1,4 +1,9 @@
 #funciones en python 
 # fuciones que se usan en listas 
 letras = ["a", "b", "n", "s", "r", "h"]
+nums = [1,2,3]
+#funcion len
 print("Longitud de la cadena:",len(letras))
+#funcion append
+nums.append(5) #agrega al final de la lista el 5
+print(nums) #imprime esto [1,2,3,5]
