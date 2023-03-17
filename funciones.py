f70c2ba45a6dadd117c2b5dbbda82737b9d33034
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-03-17 02:54:10 -0300

    mas funciones

--- funciones.py
+++ funciones.py
@@ -3,7 +3,18 @@
 letras = ["a", "b", "n", "s", "r", "h"]
 nums = [1,2,3]
 #funcion len
+print(letras)
 print("Longitud de la cadena:",len(letras))
 #funcion append
 nums.append(5) #agrega al final de la lista el 5
 print(nums) #imprime esto [1,2,3,5]
+#funcion insert 
+letras.insert(2, "j")
+print(letras)
+#funcion index
+print("Posición de la coincidencia:",letras.index("h")) #si encuentra el mismo caracter devuelve la posición de la coincidencia 
+#funcion max(list) devuelve el valor mas alto de la lista 
+#fucion min(list) devuelve el valor mas pequeño de la lista 
+# list.count(item) busca cuantas veces se repite un ítem de la lista
+# list.remove(item) elimina de la lista el item introducido en la funcion 
+# list.reverse() invierte los items de la lista 
